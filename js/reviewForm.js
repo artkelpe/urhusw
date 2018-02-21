69e80eb2c123a206c398415a9eb79381b51f4d02
artkelpe@gmail.com <artkelpe@gmail.com>
2018-02-21 22:21:15 +0100

    [new] Add sound to form

--- js/reviewForm.js
+++ js/reviewForm.js
@@ -4,26 +4,32 @@
 
 const reviewText = 'WOW, this is really great! I cant stop playing this, it is sooooo cool!';
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+let clickSound = new Audio('res/web/typewriter.wav');
 $("#inputText").on('keydown', e => {
     e.preventDefault();
     let presentText = $("#inputText").val();
-    if (presentText.length < reviewText.length)
+    if (presentText.length < reviewText.length) {
         $("#inputText").val(presentText + reviewText[presentText.length]);
+        clickSound.play();
+    }
+});
+$("#inputEmail").on('keydown', _ => {
+    clickSound.play();
 });
 
 $("button").on('click', _ => {
-   $(".alert").removeClass('alert-success').removeClass('alert-danger').html('').hide();
-   //validation
-   if (!emailRegex.test($("#inputEmail").val().toLowerCase())){
-       $(".alert").addClass('alert-danger').html('E-mail is not valid').show();
-       return;
-   }
-   if ($("#inputText").val().length < 3){
-       $(".alert").addClass('alert-danger').html('Text must contain of at least 3 symbols').show();
-       return;
-   }
 
-    $(".alert").addClass('alert-success').html('Thank you! Your review will be sent soon. Or maybe not.').show();
+    $(".alert").removeClass('alert-success').removeClass('alert-danger').html('').hide();
+    //validation
+    if (!emailRegex.test($("#inputEmail").val().toLowerCase())){
+        $(".alert").addClass('alert-danger').html('E-mail is not valid').show();
+         return;
+    }
+    if ($("#inputText").val().length < 3){
+        $(".alert").addClass('alert-danger').html('Text must contain of at least 3 symbols').show();
+        return;
+    }
+
+     $(".alert").addClass('alert-success').html('Thank you! Your review will be sent soon. Or maybe not.').show();
 
 });
